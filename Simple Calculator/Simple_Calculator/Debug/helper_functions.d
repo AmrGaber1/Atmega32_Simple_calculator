2a7defc5b42dadab9f2e3260e8b678e3ffb7ef96
Amr Gaber <amrgaber2503@gmail.com>
2023-10-05 23:25:16 +0300

    Delete Simple Calculator directory

--- Simple Calculator/Simple_Calculator/Debug/helper_functions.d
+++
@@ -1,8 +0,0 @@
-helper_functions.o helper_functions.o: ../helper_functions.c \
-  ../helper_functions.h ../mcal/std_types.h ../mcal/common_macros.h
-
-../helper_functions.h:
-
-../mcal/std_types.h:
-
-../mcal/common_macros.h:
